26fb48a639f3f0e0eea9ba0bbcffc358d773a73f
anoojkinagi <31971179+anoojkinagi@users.noreply.github.com>
2019-07-08 17:58:17 -0400

    First Commit

---
+++ Calculator/Program.cs
@@ -0,0 +1,22 @@
+﻿// Author: Anooj Kinagi
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    static class Program
+    {
+       
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
